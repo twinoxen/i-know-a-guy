e47d7625a39afc1b80ed0bbe0504c17f93f3f960
twinoxen <1854037+twinoxen@users.noreply.github.com>
2022-04-21 21:25:13 -0400

    init search

--- my-app/README.md
+++ my-app/README.md
@@ -6,3 +6,16 @@ Visit the repository for more details.
 ## Getting Started
 
 Review the generated code, and it's recommended to run a `npm init` to adjust basic project configurations before start.
+
+Copy `.env.example` to `.env`
+
+Add your twitter bearer token in `.env`
+
+run `cd my-app`
+run `npm run build`
+
+Update `SEARCH_TERM` and `DESCRIPTION_TERMS` in `dist/main.js`
+
+run `node dist/main.js`
+
+see results in `results.json`
--- my-app/src/main.ts
+++ my-app/src/main.ts
@@ -1,38 +1,51 @@
 #!/usr/bin/env node
-
-/**
- * This is a sample HTTP server.
- * Replace this with your implementation.
- */
-
+/* eslint-disable no-console */
 import 'dotenv/config'
-import { createServer, IncomingMessage, ServerResponse } from 'http'
-import { resolve } from 'path'
-import { fileURLToPath } from 'url'
-import { Config } from './config.js'
+import { twitter_client, USERNAME_REG, extract_users, clean_name } from './lib/twitter.js'
+import * as fs from 'fs/promises'
 
-const nodePath = resolve(process.argv[1])
-const modulePath = resolve(fileURLToPath(import.meta.url))
-const isCLI = nodePath === modulePath
+const SEARCH_TERM = 'miamitechweek'
+const DESCRIPTION_TERMS = 'VC,investor,angel' // comma delimited list no spaces
 
-export default function main(port: number = Config.port) {
-  const requestListener = (request: IncomingMessage, response: ServerResponse) => {
-    response.setHeader('content-type', 'text/plain;charset=utf8')
-    response.writeHead(200, 'OK')
-    response.end('Olá, Hola, Hello!')
+export const search_tweets_for_people_who = async () => {
+  try {
+    const client = twitter_client(process.env?.BEARER_TOKEN || '')
+
+    const tweets = await client.search_tweets(SEARCH_TERM)
+    const tweet_ids = tweets.data.map(tweet => tweet.id)
+
+    const usernames_mentioned_in_tweet: string[] = tweets.data
+      .filter(tweet => USERNAME_REG.test(tweet.text))
+      .flatMap(tweet => extract_users(tweet.text))
+      .map(user => clean_name(user || ''))
+
+    const liked_promises = tweet_ids.map(id => client.get_liked(id))
+    const retweeted_by_promises = tweet_ids.map(id => client.get_retweeted(id))
+
+    const responses = await Promise.all([...liked_promises, ...retweeted_by_promises])
+
+    const related_usernames = responses
+      .filter(response => response.data)
+      .flatMap(response => response.data.map(data => data.username))
+
+    const users = await client.lookup_users([...related_usernames, ...usernames_mentioned_in_tweet])
+
+    const look_for_terms = DESCRIPTION_TERMS.replace(/\s/gm, '')
+    const users_that = users.data.filter(user => {
+      return look_for_terms
+        .split(',')
+        .map(term => user.description.includes(term))
+        .some(Boolean)
+    })
+
+    await fs.writeFile('results.json', JSON.stringify(users_that))
+  } catch (e) {
+    console.log(e)
+    process.exit(1)
   }
 
-  const server = createServer(requestListener)
-
-  if (isCLI) {
-    server.listen(port)
-    // eslint-disable-next-line no-console
-    console.log(`Listening on port: ${port}`)
-  }
-
-  return server
+  console.log('Output written at: ./results.json')
+  process.exit(0)
 }
 
-if (isCLI) {
-  main()
-}
+search_tweets_for_people_who()
